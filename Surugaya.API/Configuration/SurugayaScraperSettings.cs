768ea41f1087b838b051b41add25cef17674833a
imall <h1273@evertrust.com.tw>
2025-12-02 09:28:04 +0800

    time

--- Surugaya.API/Configuration/SurugayaScraperSettings.cs
+++ Surugaya.API/Configuration/SurugayaScraperSettings.cs
@@ -14,7 +14,7 @@ public class SurugayaScraperSettings
     /// Cron 表達式 (用於 Hangfire)
     /// 預設為每 6 小時執行一次，從 12:03 開始 (12:03, 18:03, 00:03, 06:03)
     /// </summary>
-    public string CronExpression { get; set; } = "3 0/6 * * *";
+    public string CronExpression { get; set; } = "3 0,6,12,18 * * *";
 
     /// <summary>
     /// 執行超時時間 (分鐘)
